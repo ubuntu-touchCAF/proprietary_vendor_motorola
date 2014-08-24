f9aff77a172bbd698c79b6103ba522f7fdf3938d
Konsta <konsta09@gmail.com>
2014-08-24 09:51:26 +0300

    falcon: remove all apks and jars
    There's no java/dalvik in Ubuntu Touch.
    
    Conflicts:
    	falcon/falcon-vendor.mk
    	falcon/proprietary/app/MotGeoFenceSvc.apk
    	falcon/proprietary/app/TimeService.apk
    	falcon/proprietary/app/com.qualcomm.location.apk
    	falcon/proprietary/app/qcrilmsgtunnel.apk
    	falcon/proprietary/framework/qcnvitems.jar
    	falcon/proprietary/framework/qcrilhook.jar

--- falcon/falcon-vendor.mk
+++ falcon/falcon-vendor.mk
@@ -19,16 +19,6 @@ DEVICE_PACKAGE_OVERLAYS += vendor/motorola/falcon/overlay
 
 $(call inherit-product, vendor/motorola/falcon/falcon-vendor-blobs.mk)
 
-# Prebuilt APKs
+# Prebuilt vendor/libs needed for compilation
 PRODUCT_PACKAGES += \
-    com.qualcomm.location \
-    com.qualcomm.services.location \
-    MotGeoFenceSvc \
-    qcrilmsgtunnel \
-    TimeService
-
-# Prebuilt jars
-PRODUCT_PACKAGES += \
-    qcnvitems \
-    qcrilhook
-
+    libtime_genoff
